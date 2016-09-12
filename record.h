4187ab547bd514dbe155678ca9f9446e32bab5cb
Aditya Naik <naik@psu.edu>
2016-09-12 15:50:46 -0400

    changed header files

--- record.h
+++
@@ -1,9 +0,0 @@
-#ifndef RECORD_H
-#define RECORD_H
-
-
-/* function that records stuff*/
-
-int record();
-
-#endif
--- stt.h
+++
@@ -1,6 +0,0 @@
-#ifndef STT_H
-#define STT_H
-
-int speechToText(char []);
-
-#endif
